554ff7cd488036304b85f530073e07d99d496033
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-04-29 23:51:41 -0400

    finished

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact-list/contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent {
+  contactDetail:Contact = {
+        id: 0, 
+        name: "", 
+        email: "", 
+        phone: "", 
+        imageUrl: "",
+        group: null
+  };
 
+  constructor () {
+
+  }
 }
--- src/app/contacts/contact-list/contact.model.ts
+++ src/app/contacts/contact-list/contact.model.ts
@@ -5,7 +5,7 @@ export class Contact {
         public email: string, 
         public phone: string, 
         public imageUrl: string,
-        public group: string
+        public group: any
         ) {
         
     }
