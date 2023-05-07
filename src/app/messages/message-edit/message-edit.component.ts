ed23cad30e85023a297ad042172654a542198600
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-05-06 22:06:31 -0400

    Implementing messages part 1

---
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
+
+@Component({
+  selector: 'cms-message-edit',
+  templateUrl: './message-edit.component.html',
+  styleUrls: ['./message-edit.component.css']
+})
+export class MessageEditComponent implements OnInit {
+  @ViewChild('subject') subject!: ElementRef;
+  @ViewChild('msgTxt') msgTxt!: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender = "Jacob Sanchez";
+
+  constructor(){}
+  onSendMessage(){
+    const subjectValue = this.subject.nativeElement.value;
+    const msgTxt = this.msgTxt.nativeElement.value;
+
+    const message = new Message(1,subjectValue,msgTxt,this.currentSender);
+    this.addMessageEvent.emit(message);
+
+    this.onClear();
+  }
+
+  onClear(){
+    this.subject.nativeElement.value = '';
+    this.msgTxt.nativeElement.value = '';
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
