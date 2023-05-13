196f4f6574840a83c147a00524237f54abae5108
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-05-12 22:30:46 -0400

    Documents feature and dropdown

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  
+  documents: Document[] = [
+    new Document(1, "Doc 1","Doc desc 1","http://www.asdasd.com","NA"),
+    new Document(2,"Doc 2","Doc desc 2","http://www.asdasd2.com","NA"),
+    new Document(3,"Doc 3","Doc desc 3","http://www.asdasd3.com","NA"),
+    new Document(4,"Doc 4","Doc desc 4","http://www.asdasd4.com","NA"),
+  ];
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Document } from './document.model';
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
-
+  selectedDocument!: Document;
 }
